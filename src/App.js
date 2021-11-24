a20a99e4ed72cfe3185910920e2b89799288aabf
OleksiyTaranenko <oleksiytaranenko01@gmail.com>
2021-11-25 01:39:25 +0200

    react practice Widget

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [edit, setEdit] = useState(false);
+  const [person, setPerson] = useState({
+    img: '/img/worker.jpg',
+    name: 'Kirk Douglas',
+    position: 'Front-end developer',
+    status: 'On vacation'
+  });
+
+  const ref = useRef(null);
+
+  const statusList = ['On vacation', 'Absent', 'On business trip', 'Active']
+
+  const outsideClick = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      setEdit(false)
+    }
+  };
+
+  useEffect(()=> {
+    document.addEventListener('click', outsideClick);
+
+    return () => document.removeEventListener('click', outsideClick);
+  }, []);
+
+  const updatePerson = (field, value) => {
+    setPerson({...person, [field]: value})
+  };  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={ref} className={'widget ' + (edit ? 'active' : '')}>
+      <div className={'window'} onClick={()=> setEdit(!edit)}>
+        <span className={'img'} style={{backgroundImage: `url(${person.img})`}}/>
+        <div>          
+          <div className='name'>{person.name}</div>
+          <div className='position'>{person.position}</div>
+          <div className='status'>{person.status}</div>
+        </div>
+      </div>
+      {edit && (
+        <div className={'popover'}>
+          <span>Name</span>
+          <input type="text" value={person.name} onChange={(e) => updatePerson('name', e.target.value)}/>
+          <span>Position</span>
+          <input type="text" value={person.position} onChange={(e) => updatePerson('position', e.target.value)}/>
+          <span>Status</span>
+          <ul>
+            {statusList.map(status => <li className={person.status === status ? 'active' : ''} key={status} onClick={() => updatePerson('status', status)}>{status}</li>)}
+          </ul>
+        </div> 
+      )}       
     </div>
   );
 }
